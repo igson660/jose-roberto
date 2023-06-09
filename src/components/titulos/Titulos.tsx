dee4caf690958c2195d65fcd3a3a25bc6c8764da
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-06-08 20:44:22 -0500

    correções e responsividade

--- src/components/titulos/Titulos.tsx
+++ src/components/titulos/Titulos.tsx
@@ -60,12 +60,96 @@ const titulos = [
         categoria: 'Masculino / Adulto / Preta / Pluma',
         ano: '2019',
     },
+    {
+        colocação: '1° Lugar',
+        nome: 'British National IBJJF Jiu-Jitsu No-Gi Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Leve',
+        ano: '2018',
+    },
+    {
+        colocação: '3° Lugar',
+        nome: 'British National IBJJF Jiu-Jitsu Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Pena',
+        ano: '2018',
+    },
+    {
+        colocação: '2° Lugar',
+        nome: 'Spanish National IBJJF Jiu-Jitsu Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Absoluto',
+        ano: '2018',
+    },
+    {
+        colocação: '1° Lugar',
+        nome: 'Spanish National IBJJF Jiu-Jitsu No-Gi Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Leve',
+        ano: '2018',
+    },
+    {
+        colocação: '2° Lugar',
+        nome: 'Spanish National IBJJF Jiu-Jitsu Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Pena',
+        ano: '2018',
+    },
+    {
+        colocação: '2° Lugar',
+        nome: 'European IBJJF Jiu-Jitsu No-Gi Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Leve',
+        ano: '2018',
+    },
+    {
+        colocação: '3° Lugar',
+        nome: 'Rome International Open IBJJF Jiu-Jitsu Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2018',
+        categoria: 'Masculino / Master 2 / Preta / Pena',
+        ano: '2018',
+    },
+    {
+        colocação: '2° Lugar',
+        nome: 'Brasilia International Open IBJJF Jiu-Jitsu No-Gi Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2017',
+        categoria: 'Masculino / Master 1 / Preta / Pena',
+        ano: '2017',
+    },
+    {
+        colocação: '3° Lugar',
+        nome: 'Brasilia International Open IBJJF Jiu-Jitsu Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2017',
+        categoria: 'Masculino / Master 1 / Preta / Pena',
+        ano: '2017',
+    },
+    {
+        colocação: '3° Lugar',
+        nome: 'Madrid International Open IBJJF Jiu-Jitsu No-Gi Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2017',
+        categoria: 'Masculino / Master 1 / Preta / Pena',
+        ano: '2017',
+    },
+    {
+        colocação: '2° Lugar',
+        nome: 'Mexico City Summer International Open IBJJF Jiu-Jitsu Championship',
+        sub: 'IBJJF Jiu-Jitsu Championship 2017',
+        categoria: 'Masculino / Master 1 / Preta / Absoluto',
+        ano: '2017',
+    },
+    {
+        colocação: '1° Lugar',
+        nome: 'National Pro Uaejjf',
+        sub: 'Campeonato em Brasília',
+        categoria: 'Masculino / Preta',
+        ano: '2018',
+    },
 ];
 
 export const Titulos = () => {
     return (
         <>
-            <h1 className="titulo-page">Títulos</h1>
+            <h1 className="titulo-page">Premiações</h1>
             <div
                 className="titulos-container"
                 style={{ display: 'flex', flexWrap: 'wrap' }}
