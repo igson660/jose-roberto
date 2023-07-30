3b4531b70e5392b71b1d5a73b0922ed401d5798b
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-07-30 09:42:26 -0500

    inserindo tabela de horarios

--- src/components/contato/Contato.tsx
+++ src/components/contato/Contato.tsx
@@ -1,13 +1,110 @@
-import { Carousel, Typography } from 'antd';
+import { Typography, Table, Button } from 'antd';
 import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react'; // Importe o useState para gerenciar o estado
 import "./contato.css"
-import bannerHorario1 from "../../assets/img-contato/banerHorario1.jpeg"
-import bannerHorario2 from "../../assets/img-contato/bannerHorario2.jpeg"
-
 
 const { Title, Paragraph } = Typography;
 
 function Contatos() {
+    const horariosDataSource = [
+        {
+            key: uuidv4(),
+            categoria: 'Inicicante/Intermediário',
+            diaSemana: 'Seg a Sex',
+            horario: '10h30',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Iniciante/Intermediário',
+            diaSemana: 'Seg, Qua e Sex',
+            horario: '12h00',
+        },
+        {
+            key: uuidv4(),
+            categoria: "Competição",
+            diaSemana: 'Ter e Qui',
+            horario: '12h00',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Iniciante/Intermediário',
+            diaSemana: 'Seg, Qua e Sex',
+            horario: '15h00',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Avançado',
+            diaSemana: 'Seg a Sex',
+            horario: '17h20',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Iniciante',
+            diaSemana: 'Ter e Qui',
+            horario: '18h30',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Intermediário',
+            diaSemana: 'Seg, Qua e Sex',
+            horario: '18h30',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Iniciante',
+            diaSemana: 'Seg, Qua e Sex',
+            horario: '19h40',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Intermediário',
+            diaSemana: 'Ter e Qui',
+            horario: '19h40',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'Iniciante/Intermediário',
+            diaSemana: 'Seg, Qua e Sex',
+            horario: '20h40',
+        },
+        {
+            key: uuidv4(),
+            categoria: 'NO GI',
+            diaSemana: 'Ter e Qui',
+            horario: '20h40',
+        },
+    ];
+
+    const horariosColumns = [
+        {
+            title: 'Categoria',
+            dataIndex: 'categoria',
+            key: 'categoria',
+        },
+        {
+            title: 'Dia da Semana',
+            dataIndex: 'diaSemana',
+            key: 'diaSemana',
+        },
+        {
+            title: 'Horário',
+            dataIndex: 'horario',
+            key: 'horario',
+        },
+    ];
+
+    const horariosNoiteDataSource = horariosDataSource.filter(item => {
+        const horarioParts = item.horario.split(':');
+        const hora = parseInt(horarioParts[0], 10);
+        return hora >= 18;
+    });
+
+    const [mostrarTabelaNoite, setMostrarTabelaNoite] = useState(false);
+
+    const alternarTabela = () => {
+        setMostrarTabelaNoite(!mostrarTabelaNoite);
+    };
+
     return (
         <div>
             <h1 className='contato-page'>Contatos</h1>
@@ -20,23 +117,18 @@ function Contatos() {
                 <li>Local: Estrada Dias Martins, N°1397 Jardim Alan, Terceiro Andar</li>
                 <br />
             </ul>
-            <Title level={3}>Nossos Horarios</Title>
-            <Carousel>
-            <div key={uuidv4()}>
-                    <img
-                        src={bannerHorario1}
-                        alt="Imagem 1"
-                        style={{ width: '78%' }}
-                    />
-                </div>
-                <div key={uuidv4()}>
-                    <img
-                        src={bannerHorario2}
-                        alt="Imagem 2"
-                        style={{ width: '70%' }}
-                    />
-                </div>
-            </Carousel>
+            <Title level={3}>Nossos Horários</Title>
+            <Button onClick={alternarTabela} style={{ marginBottom: 16 }}>
+                {mostrarTabelaNoite ? "Mostrar Horários do Dia" : "Mostrar Horários da Noite"}
+            </Button>
+            {mostrarTabelaNoite ? (
+                <>
+                    <Title level={4}>Turno da Noite</Title>
+                    <Table dataSource={horariosNoiteDataSource} columns={horariosColumns} pagination={false} />
+                </>
+            ) : (
+                <Table dataSource={horariosDataSource} columns={horariosColumns} pagination={false} />
+            )}
         </div>
     );
 }
